23b7d5513365d0166cfa4766bc993bd019da9a7b
Illia Kryzhanovskyi <ilyuhamalyuha@gmail.com>
2025-10-21 11:34:25 +0100

    Lab_2
    Added run animation, coin as a score item. Also changed way to control inputs by using rpc

--- New Unity Project/Assets/Scripts/MultiplayerManager.cs
+++ New Unity Project/Assets/Scripts/MultiplayerManager.cs
@@ -5,17 +5,24 @@ public class MultiplayerManager : MonoBehaviour
 {
     void OnGUI()
     {
+        if(NetworkManager.Singleton == null ) return;
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
-        {
-            StartButtons();
+        try{
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
         }
-        else
+        finally
         {
-            StatusLabels();
+            GUILayout.EndArea();
         }
-        GUILayout.EndArea();
     }
+
 
     static void StartButtons()
     {
@@ -23,7 +30,7 @@ public class MultiplayerManager : MonoBehaviour
         if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
         if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
     }
-    
+
     static void StatusLabels()
     {
         var mode = NetworkManager.Singleton.IsHost ?
--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -8,39 +8,121 @@ public class PlayerMovement : NetworkBehaviour
 {
     private Animator movementAnimator;
     public float speed = 5f;
+    public int theScore;
     
     public override void OnNetworkSpawn()
     {
         movementAnimator = this.GetComponent<Animator>();
+        theScore = 0;
     }
 
     void OnCollisionEnter2D(Collision2D target)
     {
+        if(!IsSpawned) return;
         if(target.gameObject.tag.Equals("Ground") == true)
         {
             movementAnimator.SetBool("isJump", false);
         }
+
+        if (NetworkManager.Singleton == null || !NetworkManager.Singleton.IsListening)
+        return;
+
+        if(target.gameObject.tag.Equals("ScoreItem") == true)
+        {
+            if(NetworkManager.Singleton.LocalClientId == OwnerClientId)
+            {
+                scoreCollectedServerRpc(OwnerClientId);
+            }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void scoreCollectedServerRpc(ulong clientId)
+    {
+        //request players to send all their scores
+        scoreCollectedClientRpc(clientId);
+    }
+
+    [ClientRpc]
+    private void scoreCollectedClientRpc(ulong targetClientId)
+    {
+        //get the TargetClientID, compare it to the owner id and if the same - update the score
+        if (targetClientId == OwnerClientId)
+        {
+            GameObject theClientObject = this.gameObject;
+
+            NetworkManager.Singleton.ConnectedClients[OwnerClientId].PlayerObject.GetComponent
+            <PlayerMovement>().theScore =
+            NetworkManager.Singleton.ConnectedClients[OwnerClientId].PlayerObject.GetComponent
+            <PlayerMovement>().theScore + 1;
+        }
+        Debug.Log("the score of player " + targetClientId + " is " +
+        NetworkManager.Singleton.ConnectedClients[OwnerClientId].PlayerObject.GetComponent
+        <PlayerMovement>().theScore);
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (IsOwner)
         {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                HandleMovementServerRpc(1,this.NetworkObjectId);
+                // transform.position += new Vector3(speed * Time.deltaTime, 0f,0f);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                HandleMovementServerRpc(2, this.NetworkObjectId);
+                // transform.position -= new Vector3(speed * Time.deltaTime, 0f,0f);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                HandleMovementServerRpc(3, this.NetworkObjectId);
+                // transform.position += new Vector3(0f, speed * Time.deltaTime,0f);
+                // movementAnimator.SetBool("isJump", true);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                HandleMovementServerRpc(4, this.NetworkObjectId);
+                // transform.position -= new Vector3(0f, speed * Time.deltaTime,0f);
+            }
+            else { HandleMovementServerRpc(0, this.NetworkObjectId); }
+        }
+        //Debug.Log(transform.position);
+    }
+
+    [ServerRpc]
+    void HandleMovementServerRpc(int movementDirection, ulong playerID)
+    {
+        // Debug.Log("the player " + playerID + " just moves from position " +
+        // NetworkManager.Singleton.ConnectedClients[0].PlayerObject.transform.position);
+        HandleMovementClientRpc(movementDirection);
+    }
+
+    [ClientRpc]
+    void HandleMovementClientRpc(int movementDirection)
+    {
+        switch (movementDirection)
+        {
+        case 1:
             transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
+            movementAnimator.SetBool("isRun", true);
+            break;
+        case 2:
             transform.position -= new Vector3(speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
+            movementAnimator.SetBool("isRun", true);
+            break;
+        case 3:
             transform.position += new Vector3(0f, speed * Time.deltaTime, 0f);
             movementAnimator.SetBool("isJump", true);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
+            break;
+        case 4:
             transform.position -= new Vector3(0f, speed * Time.deltaTime, 0f);
+            break;
+        case 0:
+        default:
+            movementAnimator.SetBool("isRun", false);
+            break;
         }
-        Debug.Log(transform.position);
     }
 }
